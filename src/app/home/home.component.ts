20b6088a9a6ce290ac7fa2856f7915aab4c78ba1
Dibin Joseph <Dibin.Joseph@in.ey.com>
2019-08-15 00:36:27 +0530

    UI changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+import { NONE_TYPE } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('changeDivSize', [
+      state('initial', style({
+        opacity:"0",
+      })),
+      state('final', style({
+        opacity:"1",
+      })),
+      transition('initial=>final', animate('2500ms ease-in')),
+      transition('final=>initial', animate('1000ms'))
+    ]),
+  ]
 })
 export class HomeComponent implements OnInit {
 
   constructor() { }
 
+  currentState = 'initial';
+
   ngOnInit() {
+    this.changeState();
+  }
+  changeState() {
+    setTimeout(()=>
+    {
+      this.currentState = 'final'
+    },100) ;
   }
 
 }
