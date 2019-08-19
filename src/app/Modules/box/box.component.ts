ae2b9deb3d8b87cf4b7255642b8cfa6f19aa0cb7
Dibin Joseph <Dibin.Joseph@in.ey.com>
2019-08-19 14:12:54 +0530

    Home tweaks

--- src/app/Modules/box/box.component.ts
+++ src/app/Modules/box/box.component.ts
@@ -1,17 +1,45 @@
 import { Component, Input } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-box',
   templateUrl: './box.component.html',
-  styleUrls: ['./box.component.css']
+  styleUrls: ['./box.component.css'],
+  animations: [
+    trigger('changeDivSize', [
+      state('initial', style({
+        opacity:"0",
+      })),
+      state('final', style({
+        opacity:"1",
+      })),
+      transition('initial=>final', animate('2500ms ease-in')),
+      transition('final=>initial', animate('1000ms'))
+    ])]
 })
 export class BoxComponent {
   @Input() Data: string
   @Input() image: string
-  @Input() title: string
+  @Input() title: string;
+
+  currentState:any='initial';
+  elevation : string = ""
   constructor() { 
-    console.log("sdsdfsdfsd",this.image)
+    console.log("sdsdfsdfsd",this.image);
+    this.transition();
   }
+
+  transition(){
+  setTimeout(()=>
+  {
+    this.currentState = 'final'
+  },100) 
+};
+
+changeStyle($event){
+  this.elevation = $event.type == 'mouseover' ? 'shadow' : '';
+  console.log("The elevation is",this.elevation);
+}
 
 
 
