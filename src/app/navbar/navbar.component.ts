cbb8a95c68d01cdd6501161af1fa338739c27644
Aswinraj G <Aswinraj.G@in.ey.com>
2019-08-20 17:36:39 +0530

    Dashboard click

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  btnStyleGroup1: string[];
+  btnStyleGroup2: string[];
+  key1: string = 'INDUSTRIES'
+  key2: string = 'BUSINESS FUNCTIONS'
 
+  constructor() {
+    this.btnStyleGroup1 = ['btn-default','btn-default','btn-default','btn-default','btn-default'];
+    this.btnStyleGroup2 = ['btn-default','btn-default','btn-default','btn-default','btn-default','btn-default','btn-default'];
+   }
+  
   ngOnInit() {
+    localStorage.clear();
   }
 
+  public evaluateButtonGroup1(Value){
+    let previousValue = localStorage.getItem(this.key1);
+    if(previousValue != undefined){
+      this.btnStyleGroup1[ parseInt(previousValue, 10) -1] = 'btn-default'
+    }
+    localStorage.setItem(this.key1, Value);
+    this.btnStyleGroup1[Value -1] = 'btn-change';
+  }
+
+  public evaluateButtonGroup2(Value){
+    let previousValue = localStorage.getItem(this.key2);
+    if(previousValue != undefined){
+      this.btnStyleGroup2[ parseInt(previousValue, 10) -1] = 'btn-default'
+    }
+    localStorage.setItem(this.key2, Value);
+    this.btnStyleGroup2[Value -1] = 'btn-change';
+  }
 }
