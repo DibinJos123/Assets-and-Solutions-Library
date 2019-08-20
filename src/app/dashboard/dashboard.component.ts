8e1ae5a854a958435ad32f8653599e9870af63f3
Aswinraj G <Aswinraj.G@in.ey.com>
2019-08-20 21:23:47 +0530

    Dashboard complition

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,13 @@
-import { Component, OnInit, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit,OnChanges {
+export class DashboardComponent implements OnInit {
 
-  constructor() { }
-
+  
   public images = ["Group2_1.png","Group2_2.png","Group2_3.png","Group2_4.png","Group2_5.png",
                     "Group2_6.png","Group2_7.png","Group2_8.png","Group2_9.png","Group2_10.png"
                   ]
@@ -72,21 +71,220 @@ export class DashboardComponent implements OnInit,OnChanges {
   public Survey =`HR in any organization usually float surveys to employees through various channels like mail, 
   forms etc. EY’s employee satisfaction survey solution on Kaizala provides a single platform to roll out the 
   survey, engage employees, receive their responses, and provide relevant insights over it using any BI tool. `;
+  public data = [this.KYC,this.ENESA, this.SSI_Extraction,this.VendorOn_Boarding,
+                  this.KPI_Dashboard,this.Employee_Helpdesk,this.Customized_Alert,
+                  this.Invoice,this.ContractDigitization, this.Survey
+  ]
+  public r1data: []
+  public r2data: []
+  public r3data: []
 
+  public r1images: []
+  public r2images: []
+  public r3images: []
+
+  public r1titles: []
+  public r2titles: []
+  public r3titles: []
+
+  public r1urls: []
+  public r2urls: []
+  public r3urls: []
+
+
+
+  constructor() {
+    this.arraySpliter(this.data, this.images,this.titles,this.urls)
+    
+  }
   ngOnInit() {
+  }
+
+  arraySpliter(arr,arr1,arr2,arr3 ) {
+    console.log(arr2)
+    
+  this.r1data = []
+  this.r2data = []
+  this.r3data = []
+
+  this.r1images = []
+  this.r2images = []
+  this.r3images = []
+
+  this.r1titles = []
+  this.r2titles = []
+  this.r3titles = []
+
+  this.r1urls = []
+  this.r2urls = []
+  this.r3urls = []
+
+    if(arr.length > 7){
+      this.r1data = arr.slice(0,4)
+      this.r2data = arr.slice(4,8)
+      this.r3data = arr.slice(8,arr.length)
+      
+      this.r1images = arr1.slice(0,4)
+      this.r2images = arr1.slice(4,8)
+      this.r3images = arr1.slice(8,arr.length)
+
+      this.r1titles = arr2.slice(0,4)
+      this.r2titles = arr2.slice(4,8)
+      this.r3titles = arr2.slice(8,arr.length)
+
+      this.r1urls = arr3.slice(0,4)
+      this.r2urls = arr3.slice(4,8)
+      this.r3urls = arr3.slice(8,arr.length)
+
+    } else if( arr.length == 7 || (arr.length < 7 && arr.length>=4) ) {
+      this.r1data = arr.slice(0,4)
+      this.r2data = arr.slice(4,arr.length)
+      
+      this.r1images = arr1.slice(0,4)
+      this.r2images = arr1.slice(4,arr.length)
+
+      this.r1titles = arr2.slice(0,4)
+      this.r2titles = arr2.slice(4,arr.length)
+
+      this.r1urls = arr3.slice(0,4)
+      this.r2urls = arr3.slice(4,arr.length)
+
+    } else if( arr.length == 3 || (arr.length < 3 && arr.length>=0) ) {
+      this.r1data = arr.slice(0,arr.length)
+      
+      this.r1images = arr1.slice(0,arr.length)
+
+      this.r1titles = arr2.slice(0,arr.length)
+
+      this.r1urls = arr3.slice(0,arr.length)
+    }
+    console.log(this.r1titles)
+    console.log(this.r2titles)
+
+    console.log(this.r3titles)
+
+  }
+
+  async elementRemover(data,images,titles,urls,index){
+    data[index] = undefined;
+    images[index] = undefined;
+    titles[index] = undefined;
+    urls[index] = undefined;
   }
 
   group1ClickInfo: any;
   group2ClickInfo: any;
 
-  getClickInfo(Value) {
+  async getClickInfo(Value) {
     console.log(Value)
     this.group1ClickInfo = Value.Group1;
     this.group2ClickInfo = Value.Group2
-    console.log(this.group1ClickInfo, this.group2ClickInfo)
-  }
+    let visibleData = [...this.data]
+    let visibleImages = [...this.images]
+    let visibleTitles = [...this.titles]
+    let visibleUrls = [...this.urls]
+    switch(this.group1ClickInfo ) {
+      case 1: {
+        break;
+      }
+      case 2: {
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,2);
+        break;
+      }
+      case 3: {
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,0);
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,2);
+        break;
+        
+      }
+      case 4: {
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,0);
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,2);
+        break;
+      }
+      case 5: {
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,0);
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,2);
 
-  ngOnChanges(changes: SimpleChanges) {
+        break; 
+      }
+    }
+
+    switch(this.group2ClickInfo ) {
+      case 1: {
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,7);
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,8);
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,9);
+        break;
+      }
+      case 2: {
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,2);
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,3);
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,7);
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,8);
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,9);
+        break; 
+      }
+      case 3: {
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,0);
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,1);
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,2);
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,9);
+        break; 
+      }
+      case 4: {
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,0);
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,1);
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,2);
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,7);
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,8);
+        break; 
+      }
+      case 5: {
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,0);
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,1);
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,2);
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,3);
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,7);
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,8);
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,9);
+        break; 
+        
+      }
+      case 6: {
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,0);
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,1);
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,2);
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,7);
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,8);
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,9);
+        break; 
+      }
+      case 7: {
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,0);
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,1);
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,2);
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,7);
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,8);
+        await this.elementRemover(visibleData,visibleImages,visibleTitles,visibleUrls,9);
+        break; 
+      }
+    }
+    visibleData = visibleData.filter(function (el) {
+      return el != undefined;
+    });
+    visibleImages = visibleImages.filter(function (el) {
+      return el != undefined;
+    });
+    visibleTitles = visibleTitles.filter(function (el) {
+      return el != undefined;
+    });
+    visibleUrls = visibleUrls.filter(function (el) {
+      return el != undefined;
+    });
+
+
+    this.arraySpliter(visibleData, visibleImages,visibleTitles,visibleUrls)
 
   }
 
